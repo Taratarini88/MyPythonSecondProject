a2796a084780069049352e5f28d68689e39e36b8
Taratarini Patnaik <Taratarini.Patnaik@visbbsr.com>
2021-11-17 07:05:00 +0530

    my 2nd class\

--- my.py
+++ my.py
@@ -1,10 +1,27 @@
 print("Welcome to python course")
 
-print("This is another day and another code")
+print("This is another day and another code") # This is 2nd statement by Tarini
 
-print("This is third statement")
+print("This is third statement") # This is the 3rd statement written by Tara
 
 print("This is 4th statement")
 
 
 print("This is 5th statement")
+
+# the below statement 
+# will check if 5 is greater than 2 or not
+if 5 > 2:
+    print("Five is greater than two!")
+
+if 10 > 5:
+        print("10 is greater than 5")
+
+"""
+sdfkjh
+jsfdklj
+jdsalfj
+
+sdlakj
+
+"""
